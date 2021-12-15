b2c5148215a2a95c12403477a7e6f9cf957040e9
Рафаэль Халилулин <RFKhalilulin@stud.kpfu.ru>
2021-12-15 20:55:43 +0300

    Added Applied of reflection for home task 14.1 (testing), added #define preprocessor directive for the testing of tasl 14.1/ Added comments for teacher

--- Lab_13/Program.cs
+++ Lab_13/Program.cs
@@ -1,16 +1,35 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿#define DEBUG_ACCOUNT
+//Добавление дерективы предпроцессора
+using System;
+using System.Reflection;
 
 namespace Lab_13
 {
-
     class Program
     {
         static void Main(string[] args)
         {
-            // Описание лабораторной работы в файле Lab_13.docx
+            Console.WriteLine("\tExercise 14.1 ");
+
+            Account account = new Account(TypeAccount.accountCurrent, 2040000);
+            account.Withdraw(4234);
+            //Метод сработает, так как есть директива предпрцесса #define DEBUG_ACCOUNT
+            account.DumpToScreen();
+            Console.WriteLine();
+            /*Упражнение 14.2 смотреть класс Rational.cs*/
+            Console.WriteLine("\tHome Exercise 14.1 ");
+            //Применение рефлексии для обращения к метаданным класса "Building"
+            MemberInfo typeInfo = typeof(Building);
+            object[] attrs = typeInfo.GetCustomAttributes(false);
+            foreach(object attrObj in attrs)
+            {
+                if (attrObj is DeveloperInfo2Attribute attr)
+                {
+                    Console.WriteLine($"The developer of class \"Building\"  -- {attr.NameDeveloper}");
+                    Console.WriteLine($"The organization  -- {attr.NameOrganization}");
+                }
+            }
+            Console.ReadKey();
         }
     }
 }
